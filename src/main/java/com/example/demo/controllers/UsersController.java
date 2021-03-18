4e68303ebd280bca93d794b0b5771f2b78d6f81a
kunjut <v.dimnich@gmail.com>
2021-03-18 20:34:43 +0300

    change Patch/DeleteMapping to Post and routing, because edit/delete not working

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.models.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -16,7 +17,6 @@ public class UsersController {
     @Autowired
     private UserService userService;
 
-    // GET метод index по адресу /users
     @GetMapping("/admin")
     public String index(Model model) {
         // из DAO получаем всех user, пакуем в модель
@@ -24,22 +24,22 @@ public class UsersController {
         return "users/index";
     }
 
-    // GET метод show по адресу /users/:id
     @GetMapping("/admin/{id}")
-    public String show(@PathVariable("id") Long id, Model model) {
+    public String show(@PathVariable("id") Long id, Model model, Principal principal, Authentication authentication) {
         // из DAO получаем одного user по id, пакуем в модель
         model.addAttribute("user", userService.show(id));
+
+        printForMePrincipal(principal, authentication);
+
         return "users/show";
     }
 
-    // GET метод newUser по адресу /users/new
     @GetMapping("admin/new")
     public String newUser(@ModelAttribute("user") User user) {
 
         return "users/new";
     }
 
-    // POST метод create по адресу /users
     @PostMapping("/admin")
     public String create(User user) {
         userService.save(user);
@@ -47,23 +47,23 @@ public class UsersController {
         return "redirect:/admin";
     }
 
-    // GET метод edit users/:id/edit
     @GetMapping("admin/{id}/edit")
     public String edit(@PathVariable Long id, Model model) {
         model.addAttribute("user", userService.show(id));
+
         return "users/edit";
     }
 
-    // PATCH метод update users/:id
-    @PatchMapping("/admin/{id}")
+//    @PatchMapping("/admin/{id}") при переезде на boot перестал работать
+    @PostMapping("/admin/edit/{id}")
     public String update(@PathVariable("id") Long id, User user) {
         userService.update(id, user);
 
         return "redirect:/admin";
     }
 
-    // DELETE метод delete users/:id
-    @DeleteMapping("/admin/{id}")
+//    @DeleteMapping("/admin/{id}") при переезде на boot перестал работать
+    @PostMapping("/admin/delete/{id}")
     public String delete(@PathVariable("id") Long id) {
         userService.delete(id);
 
@@ -71,14 +71,25 @@ public class UsersController {
     }
 
     @GetMapping("/user")
-    public String printIfUser(ModelMap model, Principal principal) {
+    public String printIfUser(ModelMap model, Principal principal, Authentication authentication) {
         model.addAttribute("user", userService.getUserByName(principal.getName()));
-        System.out.println(principal);
+
+        printForMePrincipal(principal, authentication);
+
         return "users/show";
     }
 
     @GetMapping("/login")
     public String loginPage() {
+
         return "pages/login";
+    }
+
+    public void printForMePrincipal(Principal principal, Authentication authentication) {
+        System.out.println(principal);
+        System.out.println(userService.getUserByName(principal.getName()));
+        System.out.println(userService.getUserByName(principal.getName()).getRoles());
+        System.out.println(authentication.getName());
+        System.out.println(authentication.getAuthorities());
     }
 }
